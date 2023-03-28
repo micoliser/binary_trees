49174311ae8ac305b1ecd6be527cf9cabcd0e726
micoliser <micoliser@gmail.com>
2023-03-28 15:56:14 +0100

    more fixes

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -10,14 +10,13 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	size_t i;
-	bst_t *root, *temp;
+	bst_t *root = NULL;
 
 	for (i = 0; i < size; i++)
-	{
-		temp = bst_insert(&root, array[i]);
-		if (!temp)
-			return (NULL);
-	}
+		bst_insert(&root, array[i]);
 
-	return (root);
+	if (i == size)
+		return (root);
+
+	return (NULL);
 }
